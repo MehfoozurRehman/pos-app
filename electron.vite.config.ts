8935e4d821d3653a28e9f7ded2c0b3e8395de054
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-21 13:07:45 +0500

    Update electron.vite.config.ts

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,9 @@
-import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+//@ts-ignore // eslint-disable-next-line
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   main: {
@@ -15,6 +18,6 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react()]
+    plugins: [react(), tailwindcss()]
   }
 })
