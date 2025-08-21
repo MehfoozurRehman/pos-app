1e302104e469f0ba67afaeec4bf1080222838942
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-21 15:10:12 +0500

    Create layout.tsx

---
+++ src/renderer/src/screens/dashboard/layout.tsx
@@ -0,0 +1,26 @@
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+
+import { AppSidebar } from '@/components/app-sidebar';
+import { Outlet } from 'react-router';
+import { SiteHeader } from '@/components/site-header';
+
+export default function Layout() {
+  return (
+    <SidebarProvider
+      style={
+        {
+          '--sidebar-width': 'calc(var(--spacing) * 72)',
+          '--header-height': 'calc(var(--spacing) * 12)',
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col">
+          <Outlet />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
