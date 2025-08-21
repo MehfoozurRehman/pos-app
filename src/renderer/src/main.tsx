8e41f4564744436b82c11be5f649c44d624ce927
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-21 14:45:44 +0500

    base

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,14 +1,23 @@
 import './global.css';
 
-import App from './App';
+import { RouterProvider, createHashRouter } from 'react-router';
+
+import Home from './screens/page';
 import { StrictMode } from 'react';
 import { ThemeProvider } from '@/components/theme-provider';
 import { createRoot } from 'react-dom/client';
 
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </StrictMode>,
 );
