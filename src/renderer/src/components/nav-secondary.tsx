ba2018bb6e58e5b27278e36f1561da37b4058a7b
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-21 15:22:10 +0500

    Update nav-secondary.tsx

--- src/renderer/src/components/nav-secondary.tsx
+++ src/renderer/src/components/nav-secondary.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import { type Icon } from '@tabler/icons-react';
 
 import { SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import { Link } from 'react-router';
 
 export function NavSecondary({ items, ...props }: { items: { title: string; url: string; icon: Icon }[] } & React.ComponentPropsWithoutRef<typeof SidebarGroup>) {
   return (
@@ -13,10 +14,10 @@ export function NavSecondary({ items, ...props }: { items: { title: string; url:
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
               <SidebarMenuButton asChild>
-                <a href={item.url}>
+                <Link to={item.url}>
                   <item.icon />
                   <span>{item.title}</span>
-                </a>
+                </Link>
               </SidebarMenuButton>
             </SidebarMenuItem>
           ))}
