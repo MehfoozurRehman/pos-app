5606270d1a4c39cb856c615d54db2c85d6b13128
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-21 15:22:07 +0500

    Update nav-main.tsx

--- src/renderer/src/components/nav-main.tsx
+++ src/renderer/src/components/nav-main.tsx
@@ -1,33 +1,40 @@
-import { IconCirclePlusFilled, IconMail, type Icon } from '@tabler/icons-react';
+import { IconCirclePlusFilled, type Icon } from '@tabler/icons-react';
 
-import { Button } from '@/components/ui/button';
 import { SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import { Link, useLocation, useNavigate } from 'react-router';
 
 export function NavMain({ items }: { items: { title: string; url: string; icon?: Icon }[] }) {
+  const navigate = useNavigate();
+
+  const { pathname } = useLocation();
+
+  const isActive = (url: string) => {
+    return pathname === url;
+  };
+
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
         <SidebarMenu>
           <SidebarMenuItem className="flex items-center gap-2">
             <SidebarMenuButton
-              tooltip="Quick Create"
+              onClick={() => navigate('/dashboard')}
+              tooltip="New Order"
               className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
             >
               <IconCirclePlusFilled />
-              <span>Quick Create</span>
+              <span>New Order</span>
             </SidebarMenuButton>
-            <Button size="icon" className="size-8 group-data-[collapsible=icon]:opacity-0" variant="outline">
-              <IconMail />
-              <span className="sr-only">Inbox</span>
-            </Button>
           </SidebarMenuItem>
         </SidebarMenu>
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton tooltip={item.title}>
-                {item.icon && <item.icon />}
-                <span>{item.title}</span>
+              <SidebarMenuButton tooltip={item.title} asChild className={isActive(item.url) ? 'bg-muted text-primary-muted' : ''}>
+                <Link to={item.url}>
+                  {item.icon && <item.icon />}
+                  <span>{item.title}</span>
+                </Link>
               </SidebarMenuButton>
             </SidebarMenuItem>
           ))}
