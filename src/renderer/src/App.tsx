8087f01c88684460b8b9fbfd4962d1f9db280c5e
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-21 14:26:35 +0500

    added shadcn

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,3 +1,11 @@
-export default function App() {
-  return <div className="bg-red-500">hello</div>
+import { Button } from '@/components/ui/button'
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  )
 }
+
+export default App
