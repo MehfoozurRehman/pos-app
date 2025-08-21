2340385546f53c6defdcd27c8753eb96859ce952
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-21 15:47:01 +0500

    Update main.tsx

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,10 +2,16 @@ import './global.css';
 
 import { RouterProvider, createHashRouter } from 'react-router';
 
+import Analytics from './screens/dashboard/analytics/page';
+import Customers from './screens/dashboard/customers/page';
 import Dashboard from './screens/dashboard/page';
+import Help from './screens/dashboard/help/page';
+import Inventory from './screens/dashboard/inventory/page';
 import Layout from './screens/dashboard/layout';
 import Login from './screens/page';
 import NotFound from './screens/not-found/page';
+import Orders from './screens/dashboard/orders/page';
+import Settings from './screens/dashboard/settings/page';
 import { StrictMode } from 'react';
 import { ThemeProvider } from '@/components/theme-provider';
 import { createRoot } from 'react-dom/client';
@@ -22,6 +28,30 @@ const router = createHashRouter([
       {
         path: '',
         element: <Dashboard />,
+      },
+      {
+        path: 'orders',
+        element: <Orders />,
+      },
+      {
+        path: 'inventory',
+        element: <Inventory />,
+      },
+      {
+        path: 'customers',
+        element: <Customers />,
+      },
+      {
+        path: 'analytics',
+        element: <Analytics />,
+      },
+      {
+        path: 'settings',
+        element: <Settings />,
+      },
+      {
+        path: 'help',
+        element: <Help />,
       },
     ],
   },
