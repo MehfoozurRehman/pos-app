7c31ecbc1cee3e7010a53386b1066778c84c3f84
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-23 02:09:34 +0500

    Update env.d.ts

--- src/renderer/src/env.d.ts
+++ src/renderer/src/env.d.ts
@@ -1,1 +1,41 @@
 /// <reference types="vite/client" />
+import { electronAPI } from '@electron-toolkit/preload';
+import { DBSchema } from 'src/types';
+
+type InvokeChannel = {
+  'db:get': {
+    args: keyof DBSchema;
+    response: DBSchema[number];
+  };
+  'db:create': {
+    args: [table: keyof DBSchema, item: DBSchema[keyof DBSchema]];
+    response: DBSchema[keyof DBSchema];
+  };
+  'db:update': {
+    args: [table: keyof DBSchema, id: string, patch: Partial<DBSchema[keyof DBSchema]>];
+    response: DBSchema[keyof DBSchema] | null;
+  };
+  'db:delete': {
+    args: [table: keyof DBSchema, id: string];
+    response: boolean;
+  };
+};
+
+type ChannelName = keyof InvokeChannel;
+
+type InvokeArgs<C extends ChannelName> = InvokeChannel[C] extends { args: infer A } ? A : never;
+type InvokeResponse<C extends ChannelName> = InvokeChannel[C] extends { response: infer R } ? R : never;
+
+declare global {
+  interface Window {
+    electron: typeof electronAPI & {
+      ipcRenderer: {
+        invoke<T extends keyof DBSchema>(channel: 'db:get', table: T): Promise<DBSchema[T]>;
+        invoke<T extends keyof DBSchema>(channel: 'db:create', table: T, item: DBSchema[T][number]): Promise<DBSchema[T][number]>;
+        invoke<T extends keyof DBSchema>(channel: 'db:update', table: T, id: string, patch: Partial<DBSchema[T][number]>): Promise<DBSchema[T][number] | null>;
+        invoke<T extends keyof DBSchema>(channel: 'db:delete', table: T, id: string): Promise<boolean>;
+        invoke<C extends ChannelName>(channel: C, ...args: InvokeArgs<C>): Promise<InvokeResponse<C>>;
+      };
+    };
+  }
+}
