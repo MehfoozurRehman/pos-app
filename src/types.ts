7841306a3aa0cb61d57e5d4fc26f2e7e4a8fe254
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-23 02:10:16 +0500

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,65 @@
+type Notes = {
+  id: string;
+  title: string;
+  content: string;
+  createdAt: string;
+};
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  createdAt: string;
+};
+
+type Product = {
+  id: string;
+  name: string;
+  description: string;
+  categories: string[];
+  picture: string;
+  createdAt: string;
+};
+
+type Inventory = {
+  id: string;
+  productId: string;
+  barcode: string;
+  actualPrice: number;
+  sellingPrice: number;
+  createdAt: string;
+};
+
+type OrderItem = {
+  productId: string;
+  barcode: string;
+  discount?: number;
+};
+
+type Order = {
+  id: string;
+  orderId: string;
+  status: string;
+  customerName: string;
+  customerPhone: string;
+  items: OrderItem[];
+  discount?: number;
+  createdAt: string;
+};
+
+type Payment = {
+  id: string;
+  month: string;
+  year: string;
+  createdAt: string;
+};
+
+export type DBSchema = {
+  notes: Notes[];
+  users: User[];
+  products: Product[];
+  inventory: Inventory[];
+  orders: Order[];
+  payments: Payment[];
+};
