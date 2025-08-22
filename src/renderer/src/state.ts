ef9acd86ccda9d03faaf22d6cac3577a92f0c693
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-22 10:07:20 +0500

    Update state.ts

--- src/renderer/src/state.ts
+++ src/renderer/src/state.ts
@@ -1,4 +1,8 @@
 import { atomWithStorage } from 'jotai/utils';
+import { orders } from './data';
 
 export const cartVisibilityAtom = atomWithStorage('cartVisibility', true);
+
 export const orderQueueVisibilityAtom = atomWithStorage('orderQueueVisibility', true);
+
+export const cartAtom = atomWithStorage<(typeof orders)[number] | null>('cart', null);
