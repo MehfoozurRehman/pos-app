7a474d64aa56525d11a67a7aa2c69c299465c88b
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-22 15:41:32 +0500

    Update index.ts

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,12 +1,15 @@
 import { contextBridge } from 'electron';
 import { electronAPI } from '@electron-toolkit/preload';
 
-// Custom APIs for renderer
+declare global {
+  interface Window {
+    electron: typeof electronAPI;
+    api: typeof api;
+  }
+}
+
 const api = {};
 
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
 if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI);
@@ -15,8 +18,6 @@ if (process.contextIsolated) {
     console.error(error);
   }
 } else {
-  // @ts-ignore (define in dts)
   window.electron = electronAPI;
-  // @ts-ignore (define in dts)
   window.api = api;
 }
