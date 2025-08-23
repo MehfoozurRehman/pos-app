2e1c6a82162d8c2e45b17031eae5c7f9536ba3ba
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-23 22:50:46 +0500

    Create index.ts

---
+++ src/renderer/screens/dashboard/products/components/index.ts
@@ -0,0 +1,5 @@
+export { ProductCard } from './product-card';
+export { ProductFilters } from './product-filters';
+export { ProductForm } from './product-form';
+export { ProductsGrid } from './products-grid';
+export { ProductsHeader } from './products-header';
