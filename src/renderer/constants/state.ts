9d979713c78518b11dd556116679e3c183aed152
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-23 23:07:36 +0500

    Update state.ts

--- src/renderer/constants/state.ts
+++ src/renderer/constants/state.ts
@@ -5,5 +5,4 @@ export const cartVisibilityAtom = atomWithStorage('cartVisibility', true);
 
 export const orderQueueVisibilityAtom = atomWithStorage('orderQueueVisibility', true);
 
-// cart can hold an Order-like object from the DB; keep it unopinionated (any)
 export const cartAtom = atomWithStorage<Order | null>('cart', null);
