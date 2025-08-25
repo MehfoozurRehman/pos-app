1f11725534cc46dbd8d493128976858b85e7d4d1
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-25 14:43:02 +0500

    Create index.ts

---
+++ src/renderer/screens/dashboard/customers/components/index.ts
@@ -0,0 +1,5 @@
+export { CustomerCard } from './customer-card';
+export { CustomerDetails } from './customer-details';
+export { CustomerFilters } from './customer-filters';
+export { CustomerGrid } from './customer-grid';
+export { CustomerHeader } from './customer-header';
