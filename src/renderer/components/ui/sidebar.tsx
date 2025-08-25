63bd30ae1466326083c7fb93864f130a7362f254
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-25 17:06:20 +0500

    Update sidebar.tsx

--- src/renderer/components/ui/sidebar.tsx
+++ src/renderer/components/ui/sidebar.tsx
@@ -269,7 +269,7 @@ function SidebarInset({ className, ...props }: React.ComponentProps<'main'>) {
     <main
       data-slot="sidebar-inset"
       className={cn(
-        'bg-sidebar relative flex w-full min-[1000px]:w-[calc(100%-var(--sidebar-width))] flex-col',
+        'bg-sidebar relative flex w-full flex-1 min-[1000px]:w-[calc(100%-var(--sidebar-width))] flex-col',
         'md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow-sm md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ml-2',
         className,
       )}
