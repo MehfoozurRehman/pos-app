0da870afb5fc9a60f1e197758e11af3654f5b87c
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-25 14:32:02 +0500

    Create index.ts

---
+++ src/renderer/screens/dashboard/inventory/components/index.ts
@@ -0,0 +1,5 @@
+export { InventoryCard } from './inventory-card';
+export { InventoryFilters } from './inventory-filters';
+export { InventoryForm } from './inventory-form';
+export { InventoryGrid } from './inventory-grid';
+export { InventoryHeader } from './inventory-header';
