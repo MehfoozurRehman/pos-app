123cd717f7c52d2dcfd846c41a8c487e581d2738
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-25 14:52:38 +0500

    Create index.ts

---
+++ src/renderer/screens/dashboard/orders/components/index.ts
@@ -0,0 +1,6 @@
+export { OrderCard } from './order-card';
+export { OrderDetails } from './order-details';
+export { OrderFilters } from './order-filters';
+export { OrderGrid } from './order-grid';
+export { OrderHeader } from './order-header';
+export { OrderStats } from './order-stats';
