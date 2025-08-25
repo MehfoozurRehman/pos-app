5fccae7ed33e88ccbeb8fedc92f376f3fab4778b
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-25 14:43:19 +0500

    Update index.ts

--- src/renderer/screens/dashboard/customers/components/index.ts
+++ src/renderer/screens/dashboard/customers/components/index.ts
@@ -3,3 +3,4 @@ export { CustomerDetails } from './customer-details';
 export { CustomerFilters } from './customer-filters';
 export { CustomerGrid } from './customer-grid';
 export { CustomerHeader } from './customer-header';
+export { CustomerStats } from './customer-stats';
