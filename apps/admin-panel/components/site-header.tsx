1ba4517d672a9d168d0b917f389a0c3cf21c014a
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-26 12:17:16 +0500

    Update site-header.tsx

--- apps/admin-panel/components/site-header.tsx
+++ apps/admin-panel/components/site-header.tsx
@@ -16,14 +16,14 @@ export function SiteHeader() {
           .filter((part) => part && part !== 'dashboard')
           .filter(Boolean)
           .map((part) => part.charAt(0).toUpperCase() + part.slice(1))
-          .join(' ');
+          .join(' / ');
   }, [pathname]);
 
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <SidebarTrigger className="-ml-1" />
-        <h1 className="text-base font-medium">{pageName}</h1>
+        <h1 className="text-base font-medium">{decodeURI(pageName)}</h1>
         <div className="ml-auto flex items-center gap-2">
           <ThemeToggle />
         </div>
