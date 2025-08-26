75b557e4ed95187756289fcda0a02281c6e01895
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-26 18:08:56 +0500

    Update login-form.tsx

--- apps/pos-electron/src/renderer/components/login-form.tsx
+++ apps/pos-electron/src/renderer/components/login-form.tsx
@@ -1,19 +1,98 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Navigate, useNavigate } from 'react-router';
 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { Loader } from 'lucide-react';
+import { axios } from '@/utils/axios';
 import { cn } from '@/utils';
-import { useNavigate } from 'react-router';
+import dayjs from 'dayjs';
+import { toast } from 'sonner';
+import useSWR from 'swr';
+import useShop from '@/hooks/use-shop';
+import { useTransition } from 'react';
+
+interface Shop {
+  id: string;
+  shopId: string;
+  name: string;
+  owner: string;
+  logo: string | undefined;
+  location: string;
+  phone: string | undefined;
+  email: string | undefined;
+  description: string | undefined;
+  theme: 'light' | 'dark' | 'system' | undefined;
+  inventoryMode: 'barcode' | 'quantity' | undefined;
+}
 
 export function LoginForm({ className, ...props }: React.ComponentProps<'div'>) {
   const navigate = useNavigate();
 
+  const shop = useShop();
+
+  const [isLoggingIn, startLogin] = useTransition();
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    navigate('/dashboard');
+    const formData = new FormData(e.currentTarget);
+
+    const shopId = formData.get('shop-id');
+
+    const password = formData.get('password');
+
+    startLogin(async () => {
+      try {
+        if (!shopId) {
+          throw new Error('Missing shop id');
+        }
+
+        if (!password) {
+          throw new Error('Missing password');
+        }
+
+        const { data } = (await axios.post('/api/app-auth', {
+          shopId,
+          password,
+        })) as { data: Shop };
+
+        window.api.db.update('shop', '', {
+          name: data.name,
+          owner: data.owner,
+          logo: data.logo,
+          location: data.location,
+          phone: data.phone,
+          email: data.email,
+          description: data.description,
+          theme: data.theme,
+          inventoryMode: data.inventoryMode,
+          shopId: data.shopId,
+        });
+
+        navigate('/dashboard');
+      } catch (error) {
+        if (error instanceof Error) {
+          toast.error(error.message);
+        } else {
+          toast.error('An unexpected error occurred');
+        }
+      }
+    });
   };
+
+  if (shop.isLoading) {
+    return (
+      <div className="flex justify-center items-center h-full w-full">
+        <Loader className="animate-spin" />
+      </div>
+    );
+  }
+
+  if (shop.shopId) {
+    return <Navigate to="/dashboard" />;
+  }
 
   return (
     <div className={cn('flex flex-col gap-6', className)} {...props}>
@@ -26,15 +105,16 @@ export function LoginForm({ className, ...props }: React.ComponentProps<'div'>) 
           <form noValidate onSubmit={handleSubmit}>
             <div className="flex flex-col gap-6">
               <div className="grid gap-3">
-                <Label htmlFor="store-id">Store Id</Label>
-                <Input id="store-id" type="text" placeholder="Enter your store id" required />
+                <Label htmlFor="shop-id">shop Id</Label>
+                <Input id="shop-id" name="shop-id" type="text" placeholder="Enter your shop id" required />
               </div>
               <div className="grid gap-3">
                 <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" placeholder="Enter your password" required />
+                <Input id="password" name="password" type="password" placeholder="Enter your password" required />
               </div>
               <div className="flex flex-col gap-3">
-                <Button type="submit" className="w-full">
+                <Button disabled={isLoggingIn} type="submit" className="w-full">
+                  {isLoggingIn && <Loader className="animate-spin" />}
                   Login
                 </Button>
               </div>
