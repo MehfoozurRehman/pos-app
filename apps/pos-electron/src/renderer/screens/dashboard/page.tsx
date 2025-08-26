300c5067154575ae83b77f4fa6d4c9c0551d6684
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-26 18:11:39 +0500

    Update page.tsx

--- apps/pos-electron/src/renderer/screens/dashboard/page.tsx
+++ apps/pos-electron/src/renderer/screens/dashboard/page.tsx
@@ -4,23 +4,24 @@ import { DashboardStats } from './components/dashboard-stats';
 import { OrderDetails } from './components/order-details';
 import { OrderPanel } from './components/order-panel';
 import { ProductsPanel } from './components/products-panel';
-import { ShopHeader } from './components/shop-header';
 import { motion } from 'motion/react';
 import { useAtomValue } from 'jotai/react';
 import { useIsMobile } from '@renderer/hooks/use-mobile';
 
 export default function Dashboard() {
   const isMobile = useIsMobile();
+
   const cartVisible = useAtomValue(cartVisibilityAtom);
+
   const orderQueueVisible = useAtomValue(orderQueueVisibilityAtom);
 
   const width = isMobile ? '100%' : cartVisible ? 'calc(100% - 400px)' : '100%';
+
   const isAnyPanelInvisible = !cartVisible || !orderQueueVisible;
 
   return (
     <div className="flex flex-col h-full overflow-hidden">
       <motion.div initial={{ paddingRight: 0 }} animate={{ paddingRight: isAnyPanelInvisible ? '50px' : '0px' }} className="flex flex-col gap-4 flex-1 overflow-hidden">
-        <ShopHeader />
         <div className="flex gap-4 flex-1">
           <motion.div initial={{ width }} animate={{ width }} className="flex flex-col h-full border-r p-4 gap-4">
             <DashboardStats />
